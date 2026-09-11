878583f137d4f9ba1bcd8b08d4ffee7cf6e1d438
chennakeshavulahemapriya-creator <chennakeshavulahemapriya@gmail.com>
2026-09-12 00:17:20 +0530

    Adding Samplecode.java

---
+++ Samplecode.java
@@ -0,0 +1,6 @@
+public class Samplecode{
+    public static void main (String [] args){
+        String name = "Hemapriya Chennakeshavula";
+        System.out.println("Hello , my name is " +name);
+    }
+}
